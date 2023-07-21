24bd9fb1026d4f177e4333ae8d512107fce80769
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-07-20 21:37:30 -0700

    updates

--- http-router.js
+++ http-router.js
@@ -1,10 +1,30 @@
+const { AssetLayer } = require('dubby-sdk-test');
 
-exports.allHandler = (res, req) => {
-  /* It does Http as well */
-  res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end('Hello there!');
+const assetlayer = new AssetLayer({
+  appSecret: process.env.ASSETLAYER_APP_SECRET,
+});
+
+function abortHandler() {
+  res.aborted = true;
+};
+
+exports.allHandler = async (res, req) => {
+  res.onAborted(abortHandler);
+
+  try {
+    const app = await assetlayer.apps.getApp('633b30ca09d1acacd0c50df4');
+
+    console.log('app', app);
+
+    return res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end(JSON.stringify(app));
+  }
+  catch (e) {
+    console.log('err', e)
+  }
+
+  res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end('Hello world!');
 }
 
 exports.ballsHandler = (res, req) => {
-  /* It does Http as well */
   res.writeStatus('200 OK').writeHeader('IsBalls', 'Yes').end('Hello balls!');
 }
